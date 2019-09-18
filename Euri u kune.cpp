2acfffcf85420b8e6f1922348ccb2e9f0e48b484
ivann-Ivan <ivann876@gmail.com>
2019-09-18 12:28:51 +0200

    Add files via upload

---
+++ Euri u kune.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int a, x;
+	cout<<"Ovo je program za pretvaranje eura u kune"<<endl;
+	cout<<"Unesi iznos eura."<<endl;
+	cin>>a;
+	
+	x=a*7.38;
+	
+	cout<<"To je: "<<x<<" kuna."<<endl;
+	
+	system ("PAUSE");
+	
+	return 0;
+}
