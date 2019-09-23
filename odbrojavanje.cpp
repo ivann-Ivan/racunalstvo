d837d853df57861230fd434af3ac6b3657dc5e12
ivann-Ivan <ivann876@gmail.com>
2019-09-23 14:13:30 +0200

    Add files via upload

---
+++ odbrojavanje.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int a;
+	cout<<"Unesite broj od kojeg æe se odbrojavati:"<<endl;
+	cin>>a;
+	
+	for (;a>0; a--)
+	{
+		cout<<a<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
