f28229c4d6f3e13ece881f3347488b207cd42db8
ivann-Ivan <ivann876@gmail.com>
2019-09-23 13:46:59 +0200

    Add files via upload

---
+++ Ispis brojeva.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() 
+
+{
+	
+	int a;
+	cout<<"Ispis brojeva do 20:"<<endl;
+	for (a=1; a<20; a++)
+	{
+		
+		cout<<a<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
